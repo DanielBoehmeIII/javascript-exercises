0eb80b6d363e828a5f3192829ba14c2373d6348c
DanielBoehmeIII <172210340+DanielBoehmeIII@users.noreply.github.com>
2024-08-29 17:27:06 -0400

    Define reverseString() function to reverse params
    Used .charAt() function and .length property.
    Could have used .split("").reverse().join() but I
    think working through logically was better.
    This was a bit challenging as I was using new
    functions but I got it.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
-};
-
+const reverseString = function(string) {
+    let reverseString = "";
+    for (i = 0; i < string.length + 1; i++) {
+        reverseString += string.charAt(string.length - i);
+    }
+    return reverseString;
+}
 // Do not edit below this line
 module.exports = reverseString;
