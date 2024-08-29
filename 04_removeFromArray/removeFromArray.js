97a742837214e561c36b825a252dd39c98724ee6
DanielBoehmeIII <172210340+DanielBoehmeIII@users.noreply.github.com>
2024-08-29 18:02:23 -0400

    Define removeFromArray() function
    Used ... (rest) operator to use the rest of the
    array as a parameter. I, initially, used splice()
    to come up with a decent solution but it didn't
    pass any tests.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    let newArray = [];
+    
+    array.forEach((item) => {
+    if (!remove.includes(item)) {
+        newArray.push(item);
+    }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
