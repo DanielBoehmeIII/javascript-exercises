f16ca156df31a1dee25d3d68151e1b6dc2650a08
DanielBoehmeIII <172210340+DanielBoehmeIII@users.noreply.github.com>
2024-08-29 19:17:02 -0400

    Define convertToCelsius() and convertToFarenheit()
    I used some simple math and the Math.round()
    function to achieve this. If I can learn the
    functions in javascript that are universally used
    in other languages, then I can make a lot more
    progress.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesInFarenheit) {
+  let result = (degreesInFarenheit - 32) * (5/9);
+  result = result * 10;
+  result = Math.round(result) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesInCelcius) {
+  let result = (degreesInCelcius * (9/5)) + 32;
+  result = result * 10;
+  result = Math.round(result) / 10;
+  return result;
 };
 
 // Do not edit below this line
