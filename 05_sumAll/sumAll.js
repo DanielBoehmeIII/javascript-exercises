f3544d8927032657858a385d610f33c7e2f84517
DanielBoehmeIII <172210340+DanielBoehmeIII@users.noreply.github.com>
2024-08-29 18:57:23 -0400

    Define sumAll() function
    I used some simple conditionals and discrete math
    to solve this. It probably would have been better
    to make it modular but I'm content.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+    let result = 0;
+    if (firstNum < 0 || lastNum < 0 || firstNum % 1 != 0
+        || lastNum % 1 != 0 || typeof firstNum != "number" ||
+        typeof lastNum != "number"
+    ) {
+        return "ERROR";
+    } else if (lastNum < firstNum && lastNum > 0) {
+        if (firstNum > lastNum) {
+            for (i = 0; lastNum + i <= firstNum; i++) {
+                result += i + lastNum;
+            }
+        }
+    } else if (firstNum < lastNum && firstNum > 0) {
+        for (i = 0; i + firstNum <= lastNum; i++) {
+            result += i + firstNum;
+        }
+    } 
+    return result;
 };
 
 // Do not edit below this line
